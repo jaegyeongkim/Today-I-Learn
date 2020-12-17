c5ce4845dd48bc9ca64d8bc5be37c332a43673df
Jake <tkwk1205@naver.com>
2020-12-18 02:06:04 +0900

    Update Algorithm | 2020-12-18

--- 알고리즘 문제풀이/프로그래머스/Level 2/124 나라의 숫자.py
+++ 알고리즘 문제풀이/프로그래머스/Level 2/124 나라의 숫자.py
@@ -1,27 +1,13 @@
-
 def solution(n):
-    cnt = 0
-    num = 0
-    while n > num:
-        cnt += 1
-        num += 3**cnt
-        
     answer = ''
-    for i in range(cnt-1, -1, -1):
-        for j in range(3, 0, -1):
-            if n > j*(3**i):
-                standard = j*(3**i)
-                result = n//standard
-                check = 0
-                print(n, standard, result)
-                for k in range(j, -1, -1):
-                    check += 3**k
-                print(check)
-                if n >= standard + check:
-                    result -= 1
-                answer += str(result)                      
-                n -= result*(3**i)
-                break
-    print(n, answer)
+    while n > 0:
+        rest = n%3
+        n = n//3
+        
+        if rest == 0:
+            n -= 1
+            rest = 4
+        print(rest)
+        answer = str(rest) + answer
     
     return answer
